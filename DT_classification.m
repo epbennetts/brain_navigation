f2ed4d004bc6f82ee2ffe69bebc9960007d73b19
Eloisa Perez-Bennetts <eper8035@uni.sydney.edu.au>
2020-03-16 14:27:41 +1100

    Removed stratified cross-validation, changed accuracy metric
    -Removed SCV
    -Introduced own accuracy metric instead of inbuilt kfoldLoss
    -Looping over all 20,000 genes

--- DT_classification.m
+++ DT_classification.m
@@ -8,71 +8,79 @@ clear vars;
 [genes, target, notTarget, classes, geneNames] = filter_nans;
 [rows, cols] = size(genes);
 
-scv = cvpartition(classes,'KFold', 10, 'Stratify',true);
+%scv = cvpartition(classes,'KFold', 10, 'Stratify',true);
 
 %num of trees we want to check
-samples = 50;
-%plotting = 10;
+samples = cols;
 plotting = 1:5;
 losses = zeros(1,samples);
 margins = zeros(rows,samples);
 thresholds = zeros(samples);
 
+% tree = fitctree(genes(:,1), classes, 'MaxNumSplits', 1);
+% view(tree,'Mode','graph')
+
+
+accuracies = zeros(samples,1);
 for i = 1:samples
     gene = genes(:,i);
-    tree = fitctree(gene, classes, 'Crossval', 'on', 'CVPartition', scv, 'MaxNumSplits', 1);
-    L = kfoldLoss(tree);
-    %kfoldMargin seems to create an entire column of margins... Each
-    %element's value depending on what fold it was in
-    margin = mean(kfoldMargin(tree));
-    losses(i) = L;
-    margins(:,i) = margin;
-    %if i is in the range of elements we want to plot:
-    if ismembertol(i,plotting)
-        view(tree.Trained{1},'Mode','graph')
-        figure;
-        histogram(target(:,i), 'Normalization', 'count', 'BinWidth', 0.1);
-        title(sprintf('Gene %d with margin %g', i, margin));
-        hold on;
-        histogram(notTarget(:,i), 'Normalization', 'count', 'BinWidth', 0.1);
-        hold off;
-        legend({'target', '~target'});
+    
+    %train
+    tree = fitctree(gene, classes, 'MaxNumSplits', 1);
+    
+    %test
+    labels = predict(tree,gene);
+    
+    % calc accuracy
+    correct = zeros(rows,1);
+    for j = 1:rows
+        correct(j) = (strcmp(labels{j}, classes(j)));
     end
+    accuracies(i) = mean(correct,1);
+    
+    %calc margin
+    %margin = mean(kfoldMargin(tree));
+    %margins(:,i) = margin;
+    
+    %if i is in the range of elements we want to plot:
+        if ismembertol(i,plotting)
+            view(tree,'Mode','graph')
+            figure;
+            histogram(target(:,i), 'Normalization', 'count', 'BinWidth', 0.1);
+            title(sprintf('Gene %d with margin ', i));
+            hold on;
+            histogram(notTarget(:,i), 'Normalization', 'count', 'BinWidth', 0.1);
+            hold off;
+            legend({'target', '~target'});
+        end
 end
 
-[sorted_losses, indexOrder] = sort(losses, 'ascend');
-numGenes = size(sorted_losses, 2);
+%fix losses/accuracy first
+[sorted_accuracies, indexOrder] = sort(accuracies, 'descend');
+numGenes = size(sorted_accuracies, 1);
 numGenesArray = 1:numGenes;
-
-% plot(numGenesArray, sorted_losses, '.');
-% xlabel('num genes');
-% ylabel('kfoldLoss');
-
-%***#1 best gene *** (for now)
-bestGeneIndex = indexOrder(1);
-bestGene = string(geneNames(bestGeneIndex))
-
-
-%this is wrong
-% for i = indexOrder(1:10)
-%     view(tree.Trained{1},'Mode','graph')
-%     figure;
-%     histogram(target(:,i), 'Normalization', 'count', 'BinWidth', 0.1);
-%     title(sprintf('Gene %d with threshold ...', i));
-%     hold on;
-%     histogram(notTarget(:,i), 'Normalization', 'count', 'BinWidth', 0.1);
-%     hold off;
-%     legend({'target','~target'});
-% end
 
 
 %all genes ranked
-genes_ranked = strings(1,samples);
-for i = 1:size(indexOrder,2)
+genes_ranked = strings(samples,1);
+for i = 1:size(indexOrder,1)
     geneIndex = indexOrder(i);
     gene = string(geneNames(geneIndex));
     genes_ranked(i) = gene;
 end
 
+
 %view: losses, margins
+
+
+%plot the accuracies overall
+figure;
+plot(numGenesArray, sorted_accuracies, '.');
+xlabel('num genes');
+ylabel('accuracy');
+
+%***#1 best gene *** 
+bestGeneIndex = indexOrder(1);
+bestGene = string(geneNames(bestGeneIndex))
+
 
--- mean_test.m
+++ mean_test.m
@@ -1,5 +1,14 @@
-X = [0,0,0;
-    1,1,1;
-    2,2,2];
-a = mean(X,1)
-b = mean(X,2)
+% % X = [0,0,0;
+% %     1,1,1;
+% %     2,2,2];
+% % a = mean(X,1)
+% % b = mean(X,2)
+% 
+% x = [5 2 7 5 8];
+% X = sort(x, 'descend');
+% [Y, Z] = sort(x, 'descend');
+% 
+% 
+
+[genes, targetIndices, classes, geneNames] = filter_nans;
+target = genes()
