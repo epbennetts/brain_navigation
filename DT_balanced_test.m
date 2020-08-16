2d9fb409c366fee9710e0cf12dbb6472be529e8d
Eloisa Perez-Bennetts <eper8035@uni.sydney.edu.au>
2020-08-16 23:19:10 +1000

    Update DT_balanced_test.m
    Added second plot to make smaller histogram look bigger

--- DT_balanced_test.m
+++ DT_balanced_test.m
@@ -5,11 +5,19 @@ clear all;
 %nonTarget = rand(100000,1);
 
 %make dummy data
-targetCentre = 3;  nonTargetCentre = 0;
-targetSize = 100;  nonTargetSize = 10000;
+targetCentre = 3;  
+nonTargetCentre = 0;
+targetSize = 1000;  
+nonTargetSize = 10000;
 target = normrnd(targetCentre,1,targetSize, 1);
 nonTarget = normrnd(nonTargetCentre,1,nonTargetSize, 1);
 allData = [target; nonTarget];
+%for visualising target in histogram:
+modifier = 2; %because otherwise looks like there are more targets... Justify or just make 100% random set
+x_repetitions = nonTargetSize/targetSize/modifier; %assumes target is smallest
+y_repetitions = 1;
+target_magnified = repmat(target, x_repetitions, y_repetitions);
+
 
 %set classes
 classes = zeros(size(target,1)+size(nonTarget,1),1);
@@ -19,29 +27,56 @@ classes(1:size(target,1),:) = 1;
 costFunc = ComputeBalancedCostFunc(classes);
 
 %train
-tree = fitctree(allData, classes, 'MaxNumSplits', 1, 'cost', costFunc, 'ClassNames', [1,0]);
+tree_unbal = fitctree(allData, classes, 'MaxNumSplits', 1, 'ClassNames', [1,0]);
+tree_bal = fitctree(allData, classes, 'MaxNumSplits', 1, 'cost', costFunc, 'ClassNames', [1,0]);
 
-% Store threshold 
-threshold_raw = tree.CutPoint; %threshold with NaNs
-threshold = threshold_raw(~isnan(threshold_raw));
+% Store thresholds
+%unbalanced
+threshold_raw = tree_unbal.CutPoint; %threshold with NaNs
+threshold_unbal = threshold_raw(~isnan(threshold_raw));
+%balanced
+threshold_raw = tree_bal.CutPoint; %threshold with NaNs
+threshold_bal = threshold_raw(~isnan(threshold_raw));
 
 %test
-predictedLabels = predict(tree, allData);
+predictedLabels = predict(tree_bal, allData);
 
 %calc confusion mat & bal accuracy
 [confMat,balAcc] = ComputeConfusion(classes,predictedLabels);
 
 
-
 %view
-view(tree,'Mode','graph')
+view(tree_unbal,'Mode','graph')
+view(tree_bal,'Mode','graph')
 
-%plot histograms
-histogram(target, 'Normalization', 'count', 'BinWidth', 0.2);
-title('Gene expression of dummy targets and non-targets');
+%PLOT 1
+%gene expr histograms
+figure()
 hold on;
+title('Gene expression of dummy targets and non-targets');
+histogram(target, 'Normalization', 'count', 'BinWidth', 0.2);
 histogram(nonTarget, 'Normalization', 'count', 'BinWidth', 0.1);
 xticks(-5:1:6)
-xline(threshold, '--r', 'LineWidth', 1);
+%thresholds
+xline(threshold_unbal, '--r', 'LineWidth', 1);
+xline(threshold_bal, '--g', 'LineWidth', 1);
+%legend
+legend({'target', '~target', 'unbalanced', 'balanced'},'Location','northwest');
 hold off;
-legend({'target', '~target'});
+
+%PLOT 2 -- histograms but with target modified to look like it has similar counts
+%(Except it looks as if there are 2x as many targets because we're repeating
+%the exact same examples...)
+%gene expr histograms
+figure()
+hold on;
+title('Gene expression of dummy targets and non-targets');
+histogram(target_magnified, 'Normalization', 'count', 'BinWidth', 0.2);
+histogram(nonTarget, 'Normalization', 'count', 'BinWidth', 0.1);
+xticks(-5:1:6)
+%thresholds
+xline(threshold_unbal, '--r', 'LineWidth', 1);
+xline(threshold_bal, '--g', 'LineWidth', 1);
+%legend
+legend({'target','~target','unbalanced','balanced'}, 'Location','northwest');
+hold off;
