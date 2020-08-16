2d111153366e405f53199831125ac9a7541a76c7
Eloisa Perez-Bennetts <eper8035@uni.sydney.edu.au>
2020-08-16 23:19:37 +1000

    Create DT_AccuracyVsNoiseLevel.m
    For plotting accuracy vs noise level

---
+++ DT_AccuracyVsNoiseLevel.m
@@ -0,0 +1,51 @@
+clear all;
+close all force;
+
+%-------------------------------------------------------------------------------
+% Loads in the data, and sets up targets/nontargets for the chosen area:
+params = SetTestParams(); 
+area = params.area;
+[genes, isTarget, classes, geneNames] = filter_nans(area);
+[rows, cols] = size(genes);
+
+%see how to make SetTestParams() work without having to do area = params.area, etc. 
+params = SetTestParams(); 
+%-------------------------------------------------------------------------------
+% Parameters:
+%-------------------------------------------------------------------------------
+%"Translate" params (do this better)
+costFunction = params.costFunction;
+
+prevBestGenes = params.prevBestGenes;
+noiseStDev = params.noiseStDev;
+numNoiseIterations = params.numNoiseIterations;
+numFolds = params.numFolds;
+%-------------------------------------------------------------------------------
+% Extra Parameters:
+geneSamples = 10;
+numgenes = 1;
+noiseLevelSamples = [0:0.2:2];
+numNoiseSamples = size(noiseLevelSamples,2);
+%-------------------------------------------------------------------------------
+
+% Initialize arrays
+topAccuracy = NaN(numNoiseSamples,1);
+
+% Loop through one gene at a time
+for n = 1:numNoiseSamples
+    disp(n)
+    [indexOrder, balAcc_ranked, confMatrices_ranked, geneNames_ranked, trees_all_clean] = ...
+                    DT_classification_multiple(geneSamples, area, prevBestGenes, noiseStDev, numFolds, numNoiseIterations);
+    topAccuracy(n) = balAcc_ranked(1);
+end
+
+%-------------------------------------------------------------------------------
+% Plot accuracy vs noise:
+figure();
+plot(noiseLevelSamples, topAccuracy,'.-b');
+title(sprintf('Accuracy vs noise level in %s', area));
+xlabel('Noise level used (Std Devs)')
+ylabel('Accuracy')
+%set(gca,'xtick', 0:numgenes)
+grid on;
+
