7673e247873e96e474ec0e49b36e08d07b2b5466
Eloisa Perez-Bennetts <eper8035@uni.sydney.edu.au>
2020-09-24 18:35:41 +1000

    Update EPB_cluster_runscript.sh

--- EPB_cluster_runscript.sh
+++ EPB_cluster_runscript.sh
@@ -8,9 +8,9 @@
 #PBS -l select=1:ncpus=12:mem=8GB
 # Set your minimum acceptable walltime, format: day-hours:minutes:seconds
 #PBS -l walltime=20:00:00
-# Email user if job ends or aborts
-# PBS -m bea
-# PBS –M eper8035@uni.sydney.edu.au
+# Email user if job ends or aborts(and when it starts)
+#PBS -m ea
+#PBS –M eper8035@uni.sydney.edu.au
 #PBS -V
 
 # your commands/programs start here, for example:
