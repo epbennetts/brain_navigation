e8f0cba7bb418066f3a8ccada69a6fc75f66faf0
Eloisa Perez-Bennetts <eper8035@uni.sydney.edu.au>
2020-09-25 05:06:48 +1000

    Create runscript_cluster.sh
    main shell file

---
+++ runscript_cluster.sh
@@ -0,0 +1,22 @@
+#!/bin/csh
+# Set name of job
+#PBS -N DTBrainGenes
+#PBS -o output.txt
+#PBS -j oe
+# Specify a queue:
+# PBS -q physics
+#PBS -l select=1:ncpus=12:mem=8GB
+# Set your minimum acceptable walltime, format: day-hours:minutes:seconds
+#PBS -l walltime=20:00:00
+# Email user if job ends or aborts(and when it starts)
+# PBS -m ea
+# PBS –M eper8035@uni.sydney.edu.au
+#PBS -V
+
+# your commands/programs start here, for example:
+cd "$PBS_O_WORKDIR"
+# Show the host on which the job ran
+hostname
+# Launch the Matlab job
+module load Matlab2019b
+matlab -nodisplay -singleCompThread -r "DT_general; exit"
