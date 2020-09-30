17310fa58fffb52d18dcf9aecb6d4e284031ed60
Eloisa Perez-Bennetts <eper8035@uni.sydney.edu.au>
2020-09-30 18:33:44 +1000

    remove email function

--- runscript_AccVsNumGenes.sh
+++ runscript_AccVsNumGenes.sh
@@ -9,8 +9,8 @@
 # Set your minimum acceptable walltime, format: day-hours:minutes:seconds
 #PBS -l walltime=50:00:00
 # Email user if job ends or aborts(and when it starts)
-#PBS -m ea
-#PBS –M eper8035@uni.sydney.edu.au
+# PBS -m ea
+# PBS –M eper8035@uni.sydney.edu.au
 #PBS -V
 
 # your commands/programs start here, for example:
