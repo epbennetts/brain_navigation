f6e670396150ec631825c9eae8381b13eac7b73e
Eloisa Perez-Bennetts <eper8035@uni.sydney.edu.au>
2020-10-24 08:43:48 +1100

    Name change DT_AccVsNumGenes_plot

--- DT_AccVsNumGenes_plot.m
+++ DT_AccVsNumGenes_plot.m
@@ -3,14 +3,16 @@
 
 % ACCURACIES PLOT
 DT_plot_accuracies(maxNumGenesInDT, top_accuracies, area)
+hold on;
 
 %PLOT GENE EXPRESSION & THRESHOLDS? 
 % later in separate function:
 % %Plot thresholds
 % numplots = 5;
 % range = 'top';
+
 % %work on this functionethi
-% %DT_plot_multiple(genes, geneNames, numgenes, best_genes, isTarget, thresholds_all, samples, indexOrder, numplots, range)
+%DT_plot_multiple(genes, geneNames, maxNumGenesInDT, best_genes, isTarget, thresholds_all, samples, indexOrder, numplots, range)
 
 %end of program
 % WarnWave = [sin(1:.6:400), sin(1:.7:400), sin(1:.4:400)];
