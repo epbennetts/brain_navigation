360fe6a596035904358f12753c8ca06268ffe8e5
Eloisa Perez-Bennetts <eper8035@uni.sydney.edu.au>
2020-11-23 16:10:03 +1100

    Acc vs noise level --added error bars

--- DT_AccVsNoiseLevel.m
+++ DT_AccVsNoiseLevel.m
@@ -25,6 +25,7 @@ AccuracyVsNoise_filename = params.AccuracyVsNoise_filename;
 numNoiseIterations = params.numNoiseIterations;
 numFolds = params.numFolds;
 prevBestGenes = params.prevBestGenes;
+numGenes_temp = size(prevBestGenes,1)+1;
 %just for this script
 noiseLevelSamples = params.noiseLevelSamples;
 numNoiseSamples = size(noiseLevelSamples,2);
@@ -32,14 +33,16 @@ numNoiseSamples = size(noiseLevelSamples,2);
 
 % Initialize arrays
 topAccuracies = NaN(numNoiseSamples,1);
+topAccs_errors = NaN(numNoiseSamples,1);
 
 % Loop through different noise levels
 for n = 1:numNoiseSamples
     fprintf('(Printed from general) NOISE LEVEL iteration: %d \n', n);
     fprintf('Noise lev: %d \n', noiseLevelSamples(n));
-    [indexOrder, geneNames_ranked, balAcc_ranked, confMatrices_ranked, trees_all_clean, balAcc_stDevs_ranked] = ...
+    [indexOrder, geneNames_ranked, balAcc_ranked, confMatrices_ranked, trees_all_clean, balAcc_errors_ranked] = ...
                     DT_classification_multiple(sizeSampleSubset, area, prevBestGenes, noiseLevelSamples(n), numFolds, numNoiseIterations);
     topAccuracies(n) = balAcc_ranked(1);
+    topAccs_errors(n) = balAcc_errors_ranked(1);    
     
     %save, in case terminates:
     save(AccuracyVsNoise_filename)
--- DT_AccuracyVsNoiseLevel_plot.m
+++ DT_AccuracyVsNoiseLevel_plot.m
@@ -2,8 +2,9 @@
 
 % Plot accuracy vs noise:
 figure();
-plot(noiseLevelSamples, topAccuracies.*100,'.-b');
-title(sprintf('Accuracy vs noise level in %s', area));
+errorbar(noiseLevelSamples, topAccuracies.*100, topAccs_errors,'.-b');
+%plot(noiseLevelSamples, topAccuracies.*100,'.-b');
+title(sprintf('Accuracy vs noise level in %s (samples: %d, iters: %d, folds: %d, numGenes: %d)', area, sizeSampleSubset, numNoiseIterations, numFolds, numGenes_temp));
 xlabel('Noise level used (\sigma)')
 ylabel('Balanced Accuracy (%)')
 %set(gca,'xtick', 0:numgenes)
